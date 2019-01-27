dd72de23927f7a3f2d48890632670071422cdf13
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2019-01-27 12:12:08 -0600

    Realizando cambios en el diseño (La cague :,v)

--- src/app/components/shared/contacto/contacto.component.ts
+++ src/app/components/shared/contacto/contacto.component.ts
@@ -10,10 +10,11 @@ export class ContactoComponent implements OnInit {
   texto =
   'Devops TI es una empresa mexicana de desarrollo de software con sede en la ciudad de Tehuacán ubicada en el estado de Puebla,' +
   ' decidida a brindar el mejor servicio y trato a sus clientes' +
-  'además está comprometida a proporcionar servicios de calidad que puedan mejorar el trabajo de los demás para que tengan ' +
+  ', además está comprometida a proporcionar servicios de calidad que puedan mejorar el trabajo de los demás para que tengan ' +
   'una vida más cómoda.\n\nEn Devops TI nos regimos por una cultura de valores para cumplir con el trabajo que nuestros clientes' +
   ' nos brindan.';
   imagen = 'assets/img/oficina.jpg';
+
   constructor() { }
 
   ngOnInit() {
