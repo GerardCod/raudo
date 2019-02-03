bd62c56d7da3b81ab41baaf76a0e4d05f7d971e2
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2019-02-02 19:50:25 -0600

    Agregando librerias de la plantilla

--- src/app/components/shared/sidebar/sidebar.component.ts
+++ src/app/components/shared/sidebar/sidebar.component.ts
@@ -1,15 +1,37 @@
 import { Component, OnInit } from '@angular/core';
 
+declare const $: any;
+declare interface RouteInfo {
+    path: string;
+    title: string;
+    icon: string;
+    class: string;
+}
+export const ROUTES: RouteInfo[] = [
+    { path: '/dashboard', title: 'Inicio',  icon: 'dashboard', class: 'font-white' },
+    { path: '/user-profile', title: 'Perfil',  icon:'person', class: 'font-white' },
+    { path: '/table-list', title: 'Conductores',  icon:'commute', class: 'font-white' },
+    { path: '/typography', title: 'Typography',  icon:'library_books', class: 'font-white' },
+    { path: '/icons', title: 'Icons',  icon:'bubble_chart', class: 'font-white' },
+    { path: '/maps', title: 'Maps',  icon:'location_on', class: 'font-white' },
+    { path: '/notifications', title: 'Notifications',  icon:'notifications', class: 'font-white' }
+];
+
 @Component({
   selector: 'app-sidebar',
   templateUrl: './sidebar.component.html',
   styleUrls: ['./sidebar.component.css']
 })
 export class SidebarComponent implements OnInit {
+  menuItems: any[];
 
   constructor() { }
 
   ngOnInit() {
+    this.menuItems = ROUTES.filter(menuItem => menuItem);
   }
-
+  isMobileMenu() {
+      return $(window).width() > 991;
+  }
 }
+
