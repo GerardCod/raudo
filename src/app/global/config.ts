6d6ccb96bbface87481f4bd0c06c0d6b78c1ca46
GerardCode <gerardo.aguilar@alumno.uttehuacan.edu.mx>
2019-04-01 09:33:53 -0600

    Realizando correcciones de server

--- src/app/global/config.ts
+++ src/app/global/config.ts
@@ -1,3 +1,3 @@
 const LOCAL_URL = 'http://192.168.0.17:3000/';
 const PRODUCTION_URL = 'https://taximex-api.herokuapp.com/';
-export const APÏ_URL = LOCAL_URL;
+export const APÏ_URL = PRODUCTION_URL;
